0b2d535c35a2ec55c238be3fb514faa93f1c6fb4
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-05-17 22:01:43 +0700

    pertemuan 12🚀

---
+++ pertemuan12/Latihan2/src/Kartu.java
@@ -0,0 +1,4 @@
+public interface Kartu {
+    public boolean otentifikasi(String pin);
+    public String encode(String pin);
+}
---
+++ pertemuan12/src/Latihan1/Bentuk.java
@@ -0,0 +1,29 @@
+package Latihan1;
+
+public abstract class Bentuk {
+    protected double PHI = 3.14;
+    protected int jari2;
+
+    public Bentuk(int jari2){
+        super();
+        this.jari2 = jari2;
+    }
+    public abstract double luas();
+
+    public double getPHI() {
+        return PHI;
+    }
+
+    public int getJari2() {
+        return jari2;
+    }
+
+    public void setPHI(double PHI) {
+        this.PHI = PHI;
+    }
+
+    public void setJari2(int jari2) {
+        this.jari2 = jari2;
+    }
+
+}
